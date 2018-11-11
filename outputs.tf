aac8a209276d26bef1634839e67e05034d87bb55
Jeremiah Norris <jjno91@gmail.com>
2018-11-11 15:06:55 -0600

    correct output variable source

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "vpc_peering_connection_id" {
-  description = "https://www.terraform.io/docs/providers/aws/r/vpc_peering.html#id"
-  value       = "${aws_vpc_peering_connection.this.id}"
+  description = "https://www.terraform.io/docs/providers/aws/r/vpc_peering_accepter.html#id"
+  value       = "${aws_vpc_peering_connection_accepter.this.id}"
 }
 
 output "accept_status" {
-  description = "https://www.terraform.io/docs/providers/aws/r/vpc_peering.html#accept_status"
-  value       = "${aws_vpc_peering_connection.this.accept_status}"
+  description = "https://www.terraform.io/docs/providers/aws/r/vpc_peering_accepter.html#accept_status"
+  value       = "${aws_vpc_peering_connection_accepter.this.accept_status}"
 }
 
 output "security_group_id" {
