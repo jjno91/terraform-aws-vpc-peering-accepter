8aa8642a88439b375ca766c46aac81fe44606b7a
Benjamin Ash <bash@intelerad.com>
2020-10-22 10:55:11 -0400

    upgrade to support Terraform 0.12
    Signed-off-by: Benjamin Ash <bash@intelerad.com>

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
 output "vpc_peering_connection_id" {
   description = "https://www.terraform.io/docs/providers/aws/r/vpc_peering_accepter.html#id"
-  value       = "${element(concat(aws_vpc_peering_connection_accepter.this.*.id, list("")), 0)}"
+  value = element(
+    concat(aws_vpc_peering_connection_accepter.this.*.id, [""]),
+    0,
+  )
 }
 
 output "accept_status" {
   description = "https://www.terraform.io/docs/providers/aws/r/vpc_peering_accepter.html#accept_status"
-  value       = "${element(concat(aws_vpc_peering_connection_accepter.this.*.accept_status, list("")), 0)}"
+  value = element(
+    concat(
+      aws_vpc_peering_connection_accepter.this.*.accept_status,
+      [""],
+    ),
+    0,
+  )
 }
 
 output "security_group_id" {
   description = "Security group that grants access to and from the peer's CIDR"
-  value       = "${element(concat(aws_security_group.this.*.id, list("")), 0)}"
+  value       = element(concat(aws_security_group.this.*.id, [""]), 0)
 }
+
